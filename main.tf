bd2d08a4bef343b1049a67968d58df065d995ca4
Olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2022-02-06 03:10:56 +0000

    - I added the following resources to the networking_module/main.tf;    - aws public route table    - aws default route    - private route table    - aws route table association to link private route with default route

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 #....main module for the multi-region deployment
 
 module "us-east1_networking" {
-  source   = "./modules/networking_module"
-  vpc_cidr = "10.124.0.0/16"
-  public_subnet = "10.124.2.0/24"
+  source         = "./modules/networking_module"
+  vpc_cidr       = "10.124.0.0/16"
+  public_subnet  = "10.124.2.0/24"
   private_subnet = "10.124.1.0/24"
   providers = {
-      aws = aws.useast
+    aws = aws.useast
   }
 }
 
 module "us-west1_networking" {
-  source   = "./modules/networking_module"
-  vpc_cidr = "10.123.0.0/16"
-  public_subnet = "10.123.2.0/24"
+  source         = "./modules/networking_module"
+  vpc_cidr       = "10.123.0.0/16"
+  public_subnet  = "10.123.2.0/24"
   private_subnet = "10.123.1.0/24"
   providers = {
-      aws = aws.uswest
+    aws = aws.uswest
   }
 }
--- modules/networking_module/main.tf
+++ modules/networking_module/main.tf
@@ -1,21 +1,72 @@
 # ....modules/networking-module/main.tf
 
 resource "aws_vpc" "muyo_vpc" {
-    cidr_block = var.vpc_cidr
-    enable_dns_hostnames = "true"
-    enable_dns_support = "true"
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = "true"
+  enable_dns_support = "true"
 }
 
 resource "aws_internet_gateway" "muyo_igw" {
-    vpc_id = aws_vpc.muyo_vpc.id
+  vpc_id = aws_vpc.muyo_vpc.id
+    
+  tags = {
+    Name = "Internet-Gateway"
+  }
+
 }
 
+
+resource "aws_route_table" "muyo_public_rt" {
+  vpc_id = aws_vpc.muyo_vpc.id
+
+  tags = {
+    Name = "muyo-public-route"
+  }
+}
+
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.muyo_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.muyo_igw.id
+}
+
+
+resource "aws_default_route_table" "muyo_private_rt" {
+  default_route_table_id = aws_vpc.muyo_vpc.default_route_table_id
+
+  tags = {
+    Name = "muyo-private-route"
+  }
+}
+
+resource "aws_route_table_association" "public-rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.muyo_public_rt.id
+}
+
+
+
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.muyo_vpc.id
-    cidr_block = var.public_subnet
+  vpc_id = aws_vpc.muyo_vpc.id
+  cidr_block = var.public_subnet
+    
+    
+  tags = {
+    Name = "pub-subnet"
+  }
+
 }
 
 resource "aws_subnet" "private_subnet" {
-    vpc_id = aws_vpc.muyo_vpc.id
-    cidr_block = var.private_subnet
+  vpc_id = aws_vpc.muyo_vpc.id
+  cidr_block = var.private_subnet
+    
+    
+  tags = {
+    Name = "priv-subnet"
+  }
+
 }
+
+
